1babd31a73944bf7de60f727706803f29ea184cf
Shreyas Rai <shreyasrai23@gmail.com>
2020-08-10 20:47:12 -0700

    Extract data, annotate it (WebAnno) and convert to Spacy format

--- train_data.py
+++ train_data.py
@@ -7,6 +7,7 @@ import pytesseract
 import json
 import cv2
 import spacy
+from spacy.gold import GoldCorpus, minibatch, biluo_tags_from_offsets, tags_to_entities
 
 # For Windows only - setting the tesseract path
 pytesseract.pytesseract.tesseract_cmd = \
@@ -31,48 +32,58 @@ class Image:
 
 
 def format_json(train_data, anno_percentage: float):
+
     # Read output json file from WebAnno (Annotation tool)
     with open('webanno_annotations/image_text.json') as data_file:
         data = json.load(data_file)
+        print(type(data))
 
-    # Extract original sentences
-    sentences_list = data['_referenced_fss']['12']['sofaString'].split('\r\n')
+    # # Extract original sentences
+    # sentences_list = data['_referenced_fss']['12']['sofaString'].split('\r\n')
+
+
     # Extract entity start/ end positions and names
     ent_loc = data['_views']['_InitialView']['NamedEntity']
 
     # Extract Sentence start/ end positions
     Sentence = data['_views']['_InitialView']['Sentence']
-
-    # Split each sentence into smaller chunks
-    absolute_end = len(data['_referenced_fss']['12']['sofaString']) * anno_percentage
-    Sentence = []
-    char_count = 1
-    s_count = 0
-    while char_count <= absolute_end:
-        Sentence.append({'sofa': 12, 'begin': char_count, 'end': char_count + len(sentences_list[s_count]) + 1})
-        char_count = Sentence[-1]['end']
-        s_count += 1
-
     # Set first sentence starting position 0
     Sentence[0]['begin'] = 0
+
+    # extract original sentences:
+    sentences_list = []
+    for sent_borders in Sentence:
+        tmp_sent_string = ""
+        for letter in range(sent_borders["begin"], sent_borders["end"]):
+            tmp_sent_string += (data['_referenced_fss']['12']['sofaString'][letter])
+        sentences_list.append(tmp_sent_string)
 
     # Prepare spacy formatted training data
     ent_list = []
     for sl in range(len(Sentence)):
         ent_list_sen = []
         for el in range(len(ent_loc)):
-            if ent_loc[el]['begin'] >= Sentence[sl]['begin'] and ent_loc[el]['end'] <= Sentence[sl]['end']:
-                # Subtract entity location w/ sentence beginning- webanno generates data by treating document as a whole
-                ent_list_sen.append(
-                    [(ent_loc[el]['begin'] - Sentence[sl]['begin']), (ent_loc[el]['end'] - Sentence[sl]['begin']),
-                     ent_loc[el]['value']])
+            try:
+                if ent_loc[el]['begin'] >= Sentence[sl]['begin'] and ent_loc[el]['end'] <= Sentence[sl]['end']:
+                    # subtract entity location with sentence beginning as webanno generate data (doc as a whole)
+                    ent_list_sen.append(
+                        [(ent_loc[el]['begin'] - Sentence[sl]['begin']), (ent_loc[el]['end'] - Sentence[sl]['begin']),
+                         ent_loc[el]['value']])
+            except KeyError:
+                ent_loc[el]['begin'] = 0
+
+                if ent_loc[el]['begin'] >= Sentence[sl]['begin'] and ent_loc[el]['end'] <= Sentence[sl]['end']:
+                    # subtract entity location with sentence beginning as webanno generate data (doc as a whole)
+                    ent_list_sen.append(
+                        [(ent_loc[el]['begin'] - Sentence[sl]['begin']), (ent_loc[el]['end'] - Sentence[sl]['begin']),
+                         ent_loc[el]['value']])
+
         ent_list.append(ent_list_sen)
         # Create blank dictionary
         ent_dic = {'entities': ent_list[-1]}
         # Fill value to the dictionary
         # Prepare final training data
-        if sentences_list[sl].replace(" ", "") != "":
-            train_data.append([sentences_list[sl], ent_dic])
+        train_data.append((sentences_list[sl], ent_dic))
 
 
 def read_text(empty_ls):
@@ -96,10 +107,10 @@ def write_text(text_ls):
 def main():
     # read_text(TRAIN_DATA)
     # write_text(TRAIN_DATA)
+    nlp = spacy.blank('en', disable=["tagger", "ner"])
     format_json(TRAIN_DATA, 0.375)
     for i in TRAIN_DATA:
         print(i)
-    nlp = spacy.blank("en")
 
 
 if __name__ == '__main__':
