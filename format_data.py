daeeb863b8dc44b2f501a17e84902397c9540ebf
Shreyas Rai <shreyasrai23@gmail.com>
2020-08-10 21:21:21 -0700

    Extract data, annotate it (WebAnno) and convert to Spacy format

--- format_data.py
+++ format_data.py
@@ -6,14 +6,11 @@ import glob
 import pytesseract
 import json
 import cv2
-import spacy
-from spacy.gold import GoldCorpus, minibatch, biluo_tags_from_offsets, tags_to_entities
+
 
 # For Windows only - setting the tesseract path
 pytesseract.pytesseract.tesseract_cmd = \
     r'C:\Users\Shreyas\AppData\Local\Tesseract-OCR\tesseract.exe'
-
-TRAIN_DATA = []
 
 
 class Image:
@@ -31,59 +28,65 @@ class Image:
         self._text = set_string
 
 
-def format_json(train_data, anno_percentage: float):
-
-    # Read output json file from WebAnno (Annotation tool)
-    with open('webanno_annotations/image_text.json') as data_file:
-        data = json.load(data_file)
-        print(type(data))
-
-    # # Extract original sentences
-    # sentences_list = data['_referenced_fss']['12']['sofaString'].split('\r\n')
-
-
-    # Extract entity start/ end positions and names
-    ent_loc = data['_views']['_InitialView']['NamedEntity']
-
-    # Extract Sentence start/ end positions
-    Sentence = data['_views']['_InitialView']['Sentence']
-    # Set first sentence starting position 0
-    Sentence[0]['begin'] = 0
-
-    # extract original sentences:
+class DataFormatter:
+    train_data = []
+    Sentence = []
+    data = {}
     sentences_list = []
-    for sent_borders in Sentence:
-        tmp_sent_string = ""
-        for letter in range(sent_borders["begin"], sent_borders["end"]):
-            tmp_sent_string += (data['_referenced_fss']['12']['sofaString'][letter])
-        sentences_list.append(tmp_sent_string)
 
-    # Prepare spacy formatted training data
-    ent_list = []
-    for sl in range(len(Sentence)):
-        ent_list_sen = []
-        for el in range(len(ent_loc)):
-            try:
-                if ent_loc[el]['begin'] >= Sentence[sl]['begin'] and ent_loc[el]['end'] <= Sentence[sl]['end']:
-                    # subtract entity location with sentence beginning as webanno generate data (doc as a whole)
-                    ent_list_sen.append(
-                        [(ent_loc[el]['begin'] - Sentence[sl]['begin']), (ent_loc[el]['end'] - Sentence[sl]['begin']),
-                         ent_loc[el]['value']])
-            except KeyError:
-                ent_loc[el]['begin'] = 0
+    def __init__(self):
+        # Read output json file from WebAnno (Annotation tool)
+        with open('webanno_annotations/image_text.json') as data_file:
+            self.data = json.load(data_file)
 
-                if ent_loc[el]['begin'] >= Sentence[sl]['begin'] and ent_loc[el]['end'] <= Sentence[sl]['end']:
-                    # subtract entity location with sentence beginning as webanno generate data (doc as a whole)
-                    ent_list_sen.append(
-                        [(ent_loc[el]['begin'] - Sentence[sl]['begin']), (ent_loc[el]['end'] - Sentence[sl]['begin']),
-                         ent_loc[el]['value']])
+    def fill_sentences_ls(self):
+        for sent_borders in self.Sentence:
+            tmp_sent_string = ""
+            for letter in range(sent_borders["begin"], sent_borders["end"]):
+                tmp_sent_string += (self.data['_referenced_fss']['12']['sofaString'][letter])
+            self.sentences_list.append(tmp_sent_string)
 
-        ent_list.append(ent_list_sen)
-        # Create blank dictionary
-        ent_dic = {'entities': ent_list[-1]}
-        # Fill value to the dictionary
-        # Prepare final training data
-        train_data.append((sentences_list[sl], ent_dic))
+    def format_json(self):
+        # Extract entity start/ end positions and names
+        ent_loc = self.data['_views']['_InitialView']['NamedEntity']
+        ent_list = []
+        for sl in range(len(self.Sentence)):
+            ent_list_sen = []
+            for el in range(len(ent_loc)):
+                try:
+                    if ent_loc[el]['begin'] >= self.Sentence[sl]['begin'] and ent_loc[el]['end'] <= self.Sentence[sl]['end']:
+                        # subtract entity location with sentence beginning as webanno generate data (doc as a whole)
+                        ent_list_sen.append(
+                            [(ent_loc[el]['begin'] - self.Sentence[sl]['begin']),
+                             (ent_loc[el]['end'] - self.Sentence[sl]['begin']),
+                             ent_loc[el]['value']])
+                except KeyError:
+                    ent_loc[el]['begin'] = 0
+
+                    if ent_loc[el]['begin'] >= self.Sentence[sl]['begin'] and ent_loc[el]['end'] <= self.Sentence[sl]['end']:
+                        # subtract entity location with sentence beginning as webanno generate data (doc as a whole)
+                        ent_list_sen.append(
+                            [(ent_loc[el]['begin'] - self.Sentence[sl]['begin']),
+                             (ent_loc[el]['end'] - self.Sentence[sl]['begin']),
+                             ent_loc[el]['value']])
+            ent_list.append(ent_list_sen)
+            # Fill value to a dictionary
+            ent_dic = {'entities': ent_list[-1]}
+            # Prepare final training data
+            self.train_data.append((self.sentences_list[sl], ent_dic))
+
+    def fill_train_data(self):
+
+        # Extract Sentence start/ end positions
+        self.Sentence = self.data['_views']['_InitialView']['Sentence']
+        # Set first sentence starting position 0
+        self.Sentence[0]['begin'] = 0
+
+        # extract original sentences:
+        self.fill_sentences_ls()
+
+        # Prepare spacy formatted training data
+        self.format_json()
 
 
 def read_text(empty_ls):
@@ -105,12 +108,10 @@ def write_text(text_ls):
 
 
 def main():
-    # read_text(TRAIN_DATA)
-    # write_text(TRAIN_DATA)
-    nlp = spacy.blank('en', disable=["tagger", "ner"])
-    format_json(TRAIN_DATA, 0.375)
-    for i in TRAIN_DATA:
-        print(i)
+    df = DataFormatter()
+    read_text(df.train_data)
+    write_text(df.train_data)
+    df.fill_train_data()
 
 
 if __name__ == '__main__':
