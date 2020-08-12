6df99c0c2a797bfcfc55f2047ceca79676639519
Shreyas Rai <shreyasrai23@gmail.com>
2020-08-11 23:47:04 -0700

    Train and test the newly created custom model

---
+++ train_data.py
@@ -0,0 +1,95 @@
+import spacy
+import format_data as fd
+import random
+from spacy.util import minibatch, compounding
+from pathlib import Path
+
+# Define output folder to save new model
+model_dir = 'C:/Users/Shreyas/Documents/Python/PycharmProjects/real-estate-ner'
+
+data_format = fd.DataFormatter()
+data_format.fill_train_data()
+TRAIN_DATA = data_format.train_data
+
+
+def train_new_ner(model=None, output_dir=model_dir, n_iter=100):
+    """Load the model, set up the pipeline and train the entity recognizer."""
+    if model is not None:
+        nlp = spacy.load(model)  # load existing spaCy model
+        print("Loaded model '%s'" % model)
+    else:
+        nlp = spacy.blank("en")  # create blank Language class
+        print("Created blank 'en' model")
+    # create the built-in pipeline components and add them to the pipeline
+    # nlp.create_pipe works for built-ins that are registered with spaCy
+    if "ner" not in nlp.pipe_names:
+        ner = nlp.create_pipe("ner")
+        nlp.add_pipe(ner, last=True)
+    # otherwise, get it so we can add labels
+    else:
+        ner = nlp.get_pipe("ner")
+    # add labels
+    for _, annotations in TRAIN_DATA:
+        for ent in annotations.get("entities"):
+            ner.add_label(ent[2])
+
+    # get names of other pipes to disable them during training
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "ner"]
+    with nlp.disable_pipes(*other_pipes):  # only train NER
+        # reset and initialize the weights randomly – but only if we're
+        # training a new model
+        if model is None:
+            nlp.begin_training()
+        for itn in range(n_iter):
+            random.shuffle(TRAIN_DATA)
+            losses = {}
+            # batch up the examples using spaCy's minibatch
+            batches = minibatch(TRAIN_DATA, size=compounding(4.0, 32.0, 1.001))
+            for batch in batches:
+                texts, annotations = zip(*batch)
+                nlp.update(
+                    texts,  # batch of texts
+                    annotations,  # batch of annotations
+                    drop=0.5,  # dropout - make it harder to memorise data
+                    losses=losses,
+                )
+            print("Losses", losses)
+
+    # test the trained model
+    for text, _ in TRAIN_DATA:
+        doc = nlp(text)
+        print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
+        print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
+
+    # save model to output directory
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir()
+        nlp.to_disk(output_dir)
+        print("Saved model to", output_dir)
+
+        # test the saved model
+        print("Loading from", output_dir)
+        nlp2 = spacy.load(output_dir)
+        for text, _ in TRAIN_DATA:
+            doc = nlp2(text)
+            print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
+            print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
+
+
+def main():
+    train_new_ner()
+
+    # Use new trained saved model
+    print("Loading trained model from:", model_dir)
+    nlp2 = spacy.load(model_dir)
+    doc2 = nlp2('$750,000 3bds 2ba 2400 saft 12345 Bottleneck Drive CA 95043 House for sale')
+
+    for token in doc2:
+        print(token, token.ent_type_)
+
+
+if __name__ == '__main__':
+    main()
+
